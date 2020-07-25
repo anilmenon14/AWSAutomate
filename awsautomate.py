b4c93c5cabc8f6dc8ce1b5280878a06d2424d5e8
anilmenon14 <anil.menon14@gmail.com>
2020-07-25 17:24:31 +0400

    Using Click for CLI and updated list EC2 functions to pull up attributes

--- awsautomate.py
+++ awsautomate.py
@@ -1,8 +1,13 @@
 import boto3
+import click
+
+@click.command()
+def list_instances():
+    "List all of the EC2 instances"
+    for i in myEc2.instances.all():
+        print((' , '.join([i.id,i.instance_type,i.placement['AvailabilityZone'],i.state['Name'],i.private_dns_name])))
 
 if __name__ == "__main__":
     session = boto3.Session(profile_name="awsautomate")
     myEc2 = session.resource('ec2')
-
-    for i in myEc2.instances.all():
-        print(i)
+    list_instances();
